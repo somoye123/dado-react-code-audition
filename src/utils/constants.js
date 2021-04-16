098e0809001bd09ae43630ca4c1b6952219f6cf8
somoye123 <somoyeayotunde@gmail.com>
2021-04-16 12:12:46 +0100

    add suggested repos array to constant

---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 1,
+    text: 'django/django',
+  },
+  {
+    id: 2,
+    text: 'microsoft/vscode',
+  },
+  {
+    id: 3,
+    text: 'jezen/is-thirteen',
+  },
+  {
+    id: 4,
+    text: 'benawad/dogehouse',
+  },
+];
