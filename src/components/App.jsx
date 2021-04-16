e7c6561ba8d447c179c67100db0336f87db606b5
somoye123 <somoyeayotunde@gmail.com>
2021-04-16 11:38:23 +0100

    add searchValue state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { HomePage, CommitViewerPage } from '../pages';
 import '../App.css';
 
 export default function App() {
+  const [searchValue, setSearchValue] = useState('');
   return (
     <>
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/commit" component={CommitViewerPage} />
+        <Route exact path="/">
+          <HomePage setSearchValue={setSearchValue} />
+        </Route>
+        <Route exact path="/commit">
+          <CommitViewerPage searchValue={searchValue} />
+        </Route>
       </Switch>
     </>
   );
