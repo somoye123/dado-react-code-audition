6f20089cb3c34d44ff715500fcb37bab9847d783
somoye123 <somoyeayotunde@gmail.com>
2021-04-16 12:47:20 +0100

    pass history prop to hompage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useHistory } from 'react-router-dom';
+// eslint-disable-next-line import/no-cycle
 import { HomePage, CommitViewerPage } from '../pages';
 import '../App.css';
 
 export default function App() {
+  const history = useHistory();
   const [searchValue, setSearchValue] = useState('');
   return (
     <>
       <Switch>
         <Route exact path="/">
-          <HomePage setSearchValue={setSearchValue} />
+          <HomePage history={history} setSearchValue={setSearchValue} />
         </Route>
         <Route exact path="/commit">
           <CommitViewerPage searchValue={searchValue} />
