a9de80e795910fefe59580c7e874823e6e7be8e7
somoye123 <somoyeayotunde@gmail.com>
2021-04-16 17:58:00 +0100

    export commit details from component index

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,9 @@
+// eslint-disable-next-line import/no-cycle
 import App from './App';
 import Search from './search';
 import SuggestedRepo from './suggestedRepo';
+import CommitDetails from './commitDetails';
 
-export { App, Search, SuggestedRepo };
+export {
+  App, Search, SuggestedRepo, CommitDetails,
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-cycle
 import HomePage from './Home';
 import CommitViewerPage from './CommitViewer';
 
