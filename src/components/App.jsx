7eabf033bf1a92d4a307660a488c3d048fafc6a9
somoye123 <somoyeayotunde@gmail.com>
2021-04-16 07:25:14 +0100

    import css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { HomePage, CommitViewerPage } from '../pages';
+import '../App.css';
 
 export default function App() {
   return (
